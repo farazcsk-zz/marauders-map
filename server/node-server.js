94803efcebe237a534004d99c8f1f658bd3e465b
hibreez <4ngus.whitehead@gmail.com>
2016-12-03 21:58:02 +0000

    Adds speech password to login.

--- server/node-server.js
+++ server/node-server.js
@@ -16,6 +16,9 @@ const PORT = process.env.PORT || 8080;
 // Enable various security helpers.
 app.use(helmet());
 
+app.use('/api/speech-to-text/', require('../stt-token.js'));
+app.use('/api/text-to-speech/', require('../tts-token.js'));
+
 // API proxy logic: if you need to talk to a remote server from your client-side
 // app you can proxy it though here by editing ./proxy-config.js
 nodeProxy(app);
@@ -31,4 +34,20 @@ app.listen(PORT, (err) => {
   }
 
   winston.info(`Listening on port ${PORT}`);
+});
+
+// chrome requires https to access the user's microphone unless it's a localhost url so
+// this sets up a basic server at https://localhost3001/ using an included self-signed certificate
+// note: this is not suitable for production use
+// however bluemix automatically adds https support at http://<myapp>.mybluemix.net
+var fs = require('fs'),
+  https = require('https'),
+  HTTPS_PORT = 3001;
+
+var options = {
+  key: fs.readFileSync(__dirname + '/../keys/localhost.pem'),
+  cert: fs.readFileSync(__dirname + '/../keys/localhost.cert')
+};
+https.createServer(options, app).listen(HTTPS_PORT, function() {
+  console.log('Secure server live at https://localhost:%s/', HTTPS_PORT);
 });
